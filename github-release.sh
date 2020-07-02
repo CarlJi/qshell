3488c4b7b887938e300b31845211f0101e07791f
jichangjun <jinsdu@outlook.com>
2020-07-03 00:21:53 +0800

    github action: format release script

--- github-release.sh
+++ github-release.sh
@@ -10,7 +10,7 @@ RELEASE_NAME=$(echo $EVENT_DATA | jq -r .release.tag_name)
 PROJECT_NAME=$(basename $GITHUB_REPOSITORY)
 NAME="${NAME:-${PROJECT_NAME}-${RELEASE_NAME}}-${GOOS}-${GOARCH}"
 
-LDFLAGS='-X 'github.com/qiniu/qshell/v2/cmd.version=${RELEASE_NAME}' -extldflags '-static''
+LDFLAGS="-X 'github.com/qiniu/qshell/v2/cmd.version=${RELEASE_NAME}' -extldflags '-static'"
 CGO_ENABLED=0 go build -ldflags $LDFLAGS .
 
 ARCHIVE=tmp.tar.gz
